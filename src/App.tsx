00192d3e625b3e00e038e24d554b0f1bbcba3597
Gabriel Leão <gabrielleao335@gmail.com>
2026-04-14 19:28:57 -0400

    feat: adiciona logo do Orkut com estilização inicial via Tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
 
 function App() {
- 
-
   return (
-    <>
-     
-    </>
+    <header>
+      <div>
+        <img 
+          src="/src/assets/name.orkut.png"
+          alt="Orkut"
+          className="w-32"
+        />
+      </div>
+    </header>
   )
 }
 
